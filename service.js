f3c438b4b08c03c7e926a76a1d03b7888208cb33
Manohar Prabhu <manoharagent13@gmail.com>
2017-04-10 14:51:02 +0530

    Added param validation

--- service.js
+++ service.js
@@ -1,8 +1,21 @@
 var service = (function() {
     var database = require('./database');
+    var validateParameters = function(color, x, y) {
+        if (/^#[0-9A-F]{6}$/i.test(color) === false) {
+            return false;
+        }
+        if (x < 0 || x > 400) {
+            return false;
+        }
+        if (y < 0 || y > 400) {
+            return false;
+        }
+        return true;
+    }
     var placePixel = function(color, x, y) {
-        //TODO
-        //Check color and coordinates validity
+        if (!validateParameters(color, x, y)) {
+            return;
+        }
         database.placePixel(color, x, y);
     }
 
